b6954be97e58c41f4ee8b92d26702f1fa42a44a5
Michael Osborne <resonatortune@gmail.com>
2018-09-23 14:48:03 -0700

    Added test to SmartContractHoldsAllState

--- test/TestSmartContractHoldsAllState.sol
+++ test/TestSmartContractHoldsAllState.sol
@@ -51,4 +51,22 @@ contract TestSmartContractHoldsAllState {
         Assert.equal(actualBarcode3, 3, "Barcode3 was not set correctly");
         
     }
+
+    function testMultipleItemEntriesDifferentNames() public {
+        theContract.createItem("ITEM1", 1, 5, block.timestamp);
+        theContract.createItem("ITEM2", 2, 10, block.timestamp);
+        theContract.createItem("ITEM3", 3, 20, block.timestamp);
+
+        uint256 actualBarcode1;
+        uint256 actualBarcode2;
+        uint256 actualBarcode3;
+
+        (actualBarcode1, , ) = theContract.itemRecords(keccak256(abi.encodePacked("ITEM1")), 0);
+        (actualBarcode2, , ) = theContract.itemRecords(keccak256(abi.encodePacked("ITEM2")), 0);
+        (actualBarcode3, , ) = theContract.itemRecords(keccak256(abi.encodePacked("ITEM3")), 0);
+
+        Assert.equal(actualBarcode1, 1, "Barcode1 was not set correctly");
+        Assert.equal(actualBarcode2, 2, "Barcode2 was not set correctly");
+        Assert.equal(actualBarcode3, 3, "Barcode3 was not set correctly");
+    }
 }
