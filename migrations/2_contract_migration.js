eaed70b64347827b99c6b2c05b0adad1d507dbab
Michael Osborne <resonatortune@gmail.com>
2018-09-30 23:13:57 -0700

    Added SimpleStorage for testing, baseline of UI written

--- migrations/2_contract_migration.js
+++ migrations/2_contract_migration.js
@@ -1,7 +1,9 @@
 var SmartContractTracksItemState = artifacts.require("SmartContractTracksItemState");
 var BlockchainTracksItemState = artifacts.require("BlockchainTracksItemState");
+var SimpleStorage = artifacts.require("SimpleStorage");
 
 module.exports = function(deployer) {
     deployer.deploy(SmartContractTracksItemState);
     deployer.deploy(BlockchainTracksItemState);
+    deployer.deploy(SimpleStorage);
 };
---
+++ src/bctis-component.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import { AccountData, ContractData, ContractForm } from 'drizzle-react-components'
+
+class BctisComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+}
