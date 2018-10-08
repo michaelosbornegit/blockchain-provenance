5e83406b5edbc9f1df1874a83478c60a7d5fbe3b
Michael Osborne <resonatortune@gmail.com>
2018-10-07 21:39:27 -0700

    small changes to make it run after overhaul

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
 import ReadSimpleStorage from './ReadSimpleStorage.js'
 import WriteSimpleStorage from './WriteSimpleStorage.js'
 
