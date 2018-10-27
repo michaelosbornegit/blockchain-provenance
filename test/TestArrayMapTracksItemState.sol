955758ef06bb2ee94dd6684cdebb26e510b0b8c8
Michael Osborne <resonatortune@gmail.com>
2018-10-26 18:44:11 -0700

    Cleaned up code and added Symlink to src/contracts

--- test/TestArrayMapTracksItemState.sol
+++ test/TestArrayMapTracksItemState.sol
@@ -16,27 +16,24 @@ contract TestArrayMapTracksItemState {
 
         uint256 expectedBarcode = 1;
         uint256 expectedLocationCode = 5;
-        uint256 expectedTimeStamp = 1537684455;
 
-        theContract.updateOrAddItem("ITEM1", expectedBarcode, expectedLocationCode, expectedTimeStamp);
+        theContract.updateOrAddItem("ITEM1", expectedBarcode, expectedLocationCode);
 
         uint256 actualBarcode; 
         uint256 actualLocationCode; 
-        uint256 actualTimeStamp;
 
         // The call to the function returns a tuple(uint256,uint256,uint256) so we need to assign it to a tuple.
-        (actualBarcode, actualLocationCode, actualTimeStamp) = theContract.itemRecords(keccak256(abi.encodePacked("ITEM1")), 0);
+        (actualBarcode, actualLocationCode, ) = theContract.itemRecords(keccak256(abi.encodePacked("ITEM1")), 0);
 
         Assert.equal(actualBarcode, expectedBarcode, "Barcode was not set correctly");
         Assert.equal(actualLocationCode, expectedLocationCode, "Location code was not set correctly");
-        Assert.equal(actualTimeStamp, expectedTimeStamp, "Timestamp was not set correctly");
 
     }
 
     function testMultipleItemEntriesUnderSameIdentifier() public {
-        theContract.updateOrAddItem("ITEM1", 1, 5, block.timestamp);
-        theContract.updateOrAddItem("ITEM1", 2, 10, block.timestamp);
-        theContract.updateOrAddItem("ITEM1", 3, 20, block.timestamp);
+        theContract.updateOrAddItem("ITEM1", 1, 5);
+        theContract.updateOrAddItem("ITEM1", 2, 10);
+        theContract.updateOrAddItem("ITEM1", 3, 20);
 
         uint256 actualBarcode1;
         uint256 actualBarcode2;
@@ -53,9 +50,9 @@ contract TestArrayMapTracksItemState {
     }
 
     function testMultipleItemEntriesDifferentIdentifiers() public {
-        theContract.updateOrAddItem("ITEM1", 1, 5, block.timestamp);
-        theContract.updateOrAddItem("ITEM2", 2, 10, block.timestamp);
-        theContract.updateOrAddItem("ITEM3", 3, 20, block.timestamp);
+        theContract.updateOrAddItem("ITEM1", 1, 5);
+        theContract.updateOrAddItem("ITEM2", 2, 10);
+        theContract.updateOrAddItem("ITEM3", 3, 20);
 
         uint256 actualBarcode1;
         uint256 actualBarcode2;
@@ -74,20 +71,17 @@ contract TestArrayMapTracksItemState {
 
         uint256 expectedBarcode = 1;
         uint256 expectedLocationCode = 5;
-        uint256 expectedTimeStamp = 1537684455;
 
-        theContract.updateOrAddItem("ITEM1", expectedBarcode, expectedLocationCode, expectedTimeStamp);
+        theContract.updateOrAddItem("ITEM1", expectedBarcode, expectedLocationCode);
 
         uint256 actualBarcode; 
         uint256 actualLocationCode; 
-        uint256 actualTimeStamp;
 
         // The call to the function returns a tuple(uint256,uint256,uint256) so we need to assign it to a tuple.
-        (actualBarcode, actualLocationCode, actualTimeStamp) = theContract.getState("ITEM1", 0);
+        (actualBarcode, actualLocationCode, ) = theContract.getState("ITEM1", 0);
 
         Assert.equal(actualBarcode, expectedBarcode, "Barcode was not set correctly");
         Assert.equal(actualLocationCode, expectedLocationCode, "Location code was not set correctly");
-        Assert.equal(actualTimeStamp, expectedTimeStamp, "Timestamp was not set correctly");
 
     }
 }
