ead7afeeee1d307889ddaefd2a54c833af230d88
Michael Osborne <resonatortune@gmail.com>
2018-11-08 17:17:16 -0800

    Fixed bug in CallContractMethod, also updated packages

--- src/CallContractMethod.js
+++ src/CallContractMethod.js
@@ -22,8 +22,8 @@ class CallContractMethod extends React.Component {
         if (abi[i].name === this.props.method) {
             this.inputs = abi[i].inputs;
 
-            for (var i = 0; i < this.inputs.length; i++) {
-                initialState[this.inputs[i].name] = '';
+            for (var j = 0; j < this.inputs.length; j++) {
+                initialState[this.inputs[j].name] = '';
             }
 
             break;
@@ -49,13 +49,10 @@ class CallContractMethod extends React.Component {
     switch(true) {
         case /^uint/.test(type):
             return 'number'
-            break
         case /^string/.test(type) || /^bytes/.test(type):
             return 'text'
-            break
         case /^bool/.test(type):
             return 'checkbox'
-            break
         default:
             return 'text'
     }
